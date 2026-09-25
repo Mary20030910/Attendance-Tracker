ea1872232826033f58abd0d99928d492ec411bd4
Mary20030910 <zhouzy0910@outlook.com>
2026-09-25 11:05:29 -0400

    Update maryzhou__hw1.py

--- maryzhou__hw1.py
+++ maryzhou__hw1.py
@@ -2,3 +2,39 @@
 name: Mary Zhou
 andrewID: maryzhou
 """
+
+def parse_record(record: str) -> dict:
+    fields = record.strip().split(",")
+    if len(fields) != 4:
+        return None
+    for item in fields:
+        if item.strip() == "":
+            return None
+
+    name = fields[0].strip()
+    year = fields[1].strip()
+    event = fields[2].strip()
+    hours = fields[3].strip()
+
+    if not year.isdigit():
+        return None
+    else:
+        year = int(year)
+
+    if year < 1 or year > 4:
+        return None
+
+    try:
+        hours = float(hours)
+    except ValueError:
+        return None
+
+    if hours <= 0:
+        return None
+
+    return {
+        "name": name,
+        "year": year,
+        "event": event,
+        "hours": hours
+    }
